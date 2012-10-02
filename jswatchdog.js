f244fa4b4d252921809841a8cfdacda615d1aa72
Bevan Rudge <bevan@drupal.geek.nz>
2012-10-03 12:20:45 +1300

    Initial commit.

---
+++ jswatchdog.js
@@ -0,0 +1,48 @@
+/*jslint white: true, plusplus: true, browser: true */
+/*globals Drupal, jQuery */
+
+/**
+ * Log a system message.
+ *
+ * @see watchdog()
+ * @see http://api.drupal.org/api/drupal/includes%21bootstrap.inc/function/watchdog/6
+ *
+ * Use Drupal.watchdog.ERROR, Drupal.watchdog.WARNING etc for severity.
+ */
+Drupal.watchdog = function(type, message, variables, severity, link) {
+  "use strict";
+  var data, i;
+  data = {
+    type: 'js:' + type,
+    message: message,
+    severity: severity,
+    link: link || window.location.pathname
+  };
+
+  // Serialize the variables object.
+  for (i in variables) {
+    if (variables.hasOwnProperty(i)) {
+      data['variables[' + i +']'] = variables[i];
+    }
+  }
+
+  // Some basic flood control.  The module does this fully.
+  if (Drupal.watchdog.count < 3) {
+    jQuery.post(Drupal.settings.basePath + 'jswatchdog', data);
+  }
+
+  Drupal.watchdog.count++;
+};
+
+// Tracks
+Drupal.watchdog.count = 0;
+
+// @see watchdog_severity_levels();
+Drupal.watchdog.EMERG    = 0;
+Drupal.watchdog.ALERT    = 1;
+Drupal.watchdog.CRITICAL = 2;
+Drupal.watchdog.ERROR    = 3;
+Drupal.watchdog.WARNING  = 4;
+Drupal.watchdog.NOTICE   = 5;
+Drupal.watchdog.INFO     = 6;
+Drupal.watchdog.DEBUG    = 7;
